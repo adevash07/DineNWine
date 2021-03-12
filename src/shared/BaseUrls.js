4ec8d6207bff195e68b4f176e226342e3d9da854
adevash07 <adeyemiibrahim223@gmail.com>
2021-03-12 22:19:35 +0100

    modified

--- src/shared/BaseUrls.js
+++ src/shared/BaseUrls.js
@@ -1,3 +1,3 @@
-export const BaseUrl = "http://my-json-server.typicode.com/adevash07/json-dw/";
-export const BaseImgUrl = "http://raw.githubusercontent.com/adevash07/json-dw/master/public";
+export const BaseUrl = "https://my-json-server.typicode.com/adevash07/json-dw/";
+export const BaseImgUrl = "https://raw.githubusercontent.com/adevash07/json-dw/master/public";
 //export const BaseUrl = "http://localhost:3001/";
